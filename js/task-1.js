0ba256591418ae829bd31d5d50f4094acb1c54db
Valentyna Chudik <valentyna_chudik@ukr.net>
2020-09-26 17:05:25 +0300

    readme updated

--- js/task-1.js
+++ js/task-1.js
@@ -1,10 +1,10 @@
-const itemsRef = document.querySelectorAll(".item");
+const itemsRef = document.querySelectorAll('.item');
 console.log(`'В списке ${itemsRef.length} категории.'`);
 
-itemsRef.forEach((element) => {
-  const titleRef = element.querySelector("h2");
-  const itemCatRef = element.querySelectorAll("li");
+itemsRef.forEach(element => {
+  const titleRef = element.querySelector('h2');
+  const itemCatRef = element.querySelectorAll('li');
   console.log(
-    `Категория: ${titleRef.textContent}. Количество элементов: ${itemCatRef.length}`
+    `Категория: ${titleRef.textContent}. Количество элементов: ${itemCatRef.length}`,
   );
 });
