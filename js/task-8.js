55211286b2a40020c690c39537e4896d0bfc2570
Valentyna Chudik <valentyna_chudik@ukr.net>
2020-09-26 22:32:28 +0300

    task-8 isn't finished

---
+++ js/task-8.js
@@ -0,0 +1,29 @@
+{
+  /* <div id="controls">
+      <input type="number" min="0" max="100" step="1" />
+      <button type="button" data-action="render">Создать</button>
+      <button type="button" data-action="destroy">Очистить</button>
+    </div>
+
+    <div id="boxes"></div> */
+}
+
+// const renderBtnRef = document.querySelector('[data-action="render"]');
+// const destroyBtnRef = document.querySelector('[data-action="destroy"]');
+// const inputRef = document.querySelector('[type="number"]');
+// const boxesRef = document.querySelector('#boxes');
+
+// renderBtnRef.addEventListener('click', createBoxes);
+// destroyBtnRef.addEventListener('click');
+// let amount = 0;
+
+// function createBoxes(amount) {
+//   const divElements = [];
+
+//   for (let i = 0; i < amount; i += 1) {
+//     const element = document.createElement('div');
+//     divElements.push(element);
+//   }
+// }
+
+// boxesRef.append(...divElements);
